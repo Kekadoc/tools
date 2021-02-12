f4bf09dff382e2822b393f0c328e1cb85493ee8b
qeGame <QeGame69@gmail.com>
2021-02-12 14:11:06 +0500

    12.02.2021 DataStatesCollector.kt

--- app/src/main/java/com/kekadoc/tools/MainActivity.kt
+++ app/src/main/java/com/kekadoc/tools/MainActivity.kt
@@ -1,55 +1,236 @@
 package com.kekadoc.tools
 
+import android.annotation.SuppressLint
+import android.graphics.Color
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import com.kekadoc.tools.builder.AbstractBuilder
-import com.kekadoc.tools.data.DataAggregate
-import com.kekadoc.tools.data.DataStatesCollector
-import com.kekadoc.tools.value.ValueUtils
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.kekadoc.tools.android.dpToPx
+import com.kekadoc.tools.android.shaper.ShapedDrawableBuilder
+import com.kekadoc.tools.android.shaper.ShapedDrawableBuilder.Companion.roundAllCorners
+import com.kekadoc.tools.android.view.ViewById
+import com.kekadoc.tools.android.view.ViewUtils.findAllViews
+import com.kekadoc.tools.data.state.StateKeeper
+import com.kekadoc.tools.data.state.dataStatesCollector
+import com.kekadoc.tools.observer.Observing
 
+@SuppressLint("NonConstantResourceId")
 class MainActivity : AppCompatActivity() {
 
     companion object {
         private const val TAG: String = "MainActivity-TAG"
     }
 
+    @ViewById(id = R.id.view_0)
+    lateinit var view0: View
+    @ViewById(id = R.id.view_1)
+    lateinit var view1: View
+    @ViewById(id = R.id.view_2)
+    lateinit var view2: View
+
+    @ViewById(id = R.id.recyclerView)
+    lateinit var recyclerView: RecyclerView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findAllViews()
 
-        val q = ObjectUtils.requireNonNull(1)
+        val focusability = ViewFocusability.Auto.onClick<View>().apply {
+            addObserver(
+                    onFocus = {
+                        it.animate()
+                                .scaleY(1.2f)
+                                .scaleX(1.2f)
+                                .alpha(1f)
+                                .setDuration(200L)
+                                .start()
+                    },
+                    onNormal = {
+                        it.animate()
+                                .scaleY(1.0f)
+                                .scaleX(1.0f)
+                                .alpha(1f)
+                                .setDuration(200L)
+                                .start()
+                    },
+                    onHide = {
+                        it.animate()
+                                .scaleY(0.8f)
+                                .scaleX(0.8f)
+                                .alpha(0.6f)
+                                .setDuration(200L)
+                                .start()
+                    }
+            )
+            add(view0)
+            add(view1)
+            add(view2)
+        }
 
-        val from = 0L
-        val to = 100L
-        val current = 90L
+        val adapter = Adapter()
+        val data = arrayListOf<Message>()
+        for (i in 0..20) data.add(Message("Message #$i"))
 
-        val events = object : ValueUtils.RangeChangeEvents<Long> {
-            override fun onChange(min: Long, max: Long, current: Long, change: Long) {
-                Log.e(TAG, "onChange: min = $min; max = $max; current = $current; change = $change")
-            }
-            override fun onOverflow(min: Long, max: Long, current: Long, overflow: Long) {
-                Log.e(TAG, "onOverflow: min = $min; max = $max; current = $current; overflow = $overflow")
-            }
-            override fun onMax(min: Long, max: Long) {
-                Log.e(TAG, "onMax: min = $min; max = $max")
-            }
-            override fun onMin(min: Long, max: Long) {
-                Log.e(TAG, "onMin: min = $min; max = $max")
+        adapter.submitList(data)
+
+        recyclerView.adapter = adapter
+        recyclerView.addItemDecoration(
+                SpacesItemDecoration(
+                        left = dpToPx(16f).toInt(),
+                        top = dpToPx(16f).toInt(),
+                        right = dpToPx(16f).toInt(),
+                        bottom = dpToPx(16f).toInt(),
+                )
+        )
+
+    }
+
+    private class VH(itemView: TextView) : RecyclerView.ViewHolder(itemView) {
+
+        var data: StateKeeper<Message, Colors>? = null
+        var observing: Observing? = null
+
+        fun invokeAction() {
+            Log.e(TAG, "invokeAction: $data")
+            data?.let {
+                it.state = it.state.getNext()
             }
         }
 
-        ValueUtils.addValueInRange(from, to, current, 20, events)
-
-        val dataAggregate = object : DataAggregate() {
-            override fun createData(index: Int) {
-                Log.e(TAG, "createData: $index")
-            }
-            override fun removeData(index: Int) {
-                Log.e(TAG, "removeData: $index")
+        fun inject(data: StateKeeper<Message, Colors>) {
+            this.data = data.apply {
+                (itemView as TextView).text = this.data.message
+                observing?.remove()
+                observing = observe { oldState, newState ->
+                    Log.e(TAG, "inject: ")
+                    itemView.background = createDrawable(newState.color)
+                }
             }
         }
-        dataAggregate.notifyData(5, 3)
+
+        fun createDrawable(color: Int): Drawable {
+            return ShapedDrawableBuilder.createWithContext(itemView.context) {
+                setTint(color)
+                setElevation(itemView.context.dpToPx(8f))
+                setShadowColor(Color.BLACK)
+                setRippleColor(Color.RED)
+                setAlpha(255)
+                roundAllCorners(dpToPx(16f))
+            }.build()
+        }
+
+        fun focus() {
+            itemView.animate()
+                    .scaleX(1.1f)
+                    .scaleY(1.1f)
+                    .alpha(1f)
+                    .setDuration(200L)
+                    .start()
+        }
+        fun hide() {
+            itemView.animate()
+                    .scaleX(0.9f)
+                    .scaleY(0.9f)
+                    .alpha(0.6f)
+                    .setDuration(200L)
+                    .start()
+        }
+        fun reset() {
+            itemView.animate()
+                    .scaleX(1.0f)
+                    .scaleY(1.0f)
+                    .alpha(1f)
+                    .setDuration(200L)
+                    .start()
+        }
+
+    }
+
+    private class Adapter : ListAdapter<Message, VH>(DiffCallback()) {
+
+        companion object {
+            const val STATE_NULL = -1
+            const val STATE_NORMAL = 0
+            const val STATE_FOCUS = 1
+            const val STATE_HIDE = 2
+        }
+
+        private val messageStates = dataStatesCollector<Message, Colors>() { oldState, newState ->
+            Log.e(TAG, "onStateChange old: $oldState new: $newState")
+        }
+
+        override fun onCurrentListChanged(
+            previousList: MutableList<Message>, currentList: MutableList<Message>) {
+
+            messageStates.clear()
+            currentList.forEach { messageStates.add(it, Colors.WHITE) }
+
+        }
+
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+            val context = parent.context
+            val view = TextView(context).apply {
+                textAlignment = TextView.TEXT_ALIGNMENT_CENTER
+                isClickable = true
+                if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.O) {
+                    focusable = View.FOCUSABLE
+                }
+            }
+            val lp = RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, context.dpToPx(56f).toInt())
+            view.layoutParams = lp
+
+            val vh = VH(view)
+
+            view.setOnClickListener {
+                vh.invokeAction()
+            }
+
+            return vh
+        }
+        override fun onBindViewHolder(holder: VH, position: Int) {
+            val message = getItem(position)
+            holder.inject(messageStates.getStateKeeper(message)!!)
+        }
+
+    }
+
+    private class DiffCallback : DiffUtil.ItemCallback<Message>() {
+        override fun areItemsTheSame(oldItem: Message, newItem: Message): Boolean {
+            return oldItem === newItem
+        }
+        override fun areContentsTheSame(oldItem: Message, newItem: Message): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+}
+
+data class Message(val message: String)
+enum class Colors(val color: Int) {
+
+    RED(Color.RED),
+    GREEN(Color.GREEN),
+    BLUE(Color.BLUE),
+    BLACK(Color.BLACK),
+    WHITE(Color.WHITE);
+
+    fun getNext(): Colors {
+        return when (this) {
+            RED -> GREEN
+            GREEN -> BLUE
+            BLUE -> BLACK
+            BLACK -> WHITE
+            WHITE -> RED
+        }
     }
 
 }
---
+++ tools/src/main/java/com/kekadoc/tools/data/state/DataStatesCollector.kt
@@ -0,0 +1,133 @@
+package com.kekadoc.tools.data.state
+
+import com.kekadoc.tools.observer.Observing
+
+fun <Data, State, Keeper: StateKeeper<Data, State>>
+        dataStatesCollector(keeper: DataStatesCollector<Data, State, Keeper>.() -> Keeper):
+        DataStatesCollector<Data, State, Keeper> {
+    return object : DataStatesCollector<Data, State, Keeper>() {
+        override fun onCreateStateKeeper(data: Data, state: State): Keeper {
+            return keeper.invoke(this)
+        }
+    }
+}
+
+fun <Data, State> dataStatesCollector():
+        DataStatesCollector<Data, State, StateKeeper.Default<Data, State>> {
+    return DataStatesCollector.Simple()
+}
+
+fun <Data, State> dataStatesCollector(observer: (oldState: State, newState: State) -> Unit):
+        DataStatesCollector<Data, State, StateKeeper.Default<Data, State>> {
+    return object : DataStatesCollector.Simple<Data, State>() {
+        override fun onDataStateChange(
+            keeper: StateKeeper.Default<Data, State>,
+            oldState: State,
+            newState: State
+        ) {
+            observer.invoke(oldState, newState)
+        }
+    }
+}
+
+
+abstract class DataStatesCollector<Data, State, Keeper : StateKeeper<Data, State>> {
+
+    protected fun Data.isState(state: State): Boolean {
+        return getStateKeeper(this)?.state == state
+    }
+
+    private val states = hashMapOf<Data, Keeper>()
+
+    fun getStateKeeper(data: Data): Keeper? = states[data]
+    fun getAllData(): List<Data> = states.keys.toList()
+    fun getAll(): Collection<Keeper> = states.values
+    fun clear() {
+        getAll().forEach { remove(it) }
+    }
+    fun add(data: Data, state: State): Keeper {
+        var keeper = getStateKeeper(data)
+
+        if (keeper == null) {
+            keeper = onCreateStateKeeper(data, state).apply {
+                observe { oldState, newState ->
+                    onDataStateChange(this, oldState, newState)
+                }
+            }
+            this.states[data] = keeper
+            onAdded(keeper)
+        }
+
+        return keeper
+    }
+    fun remove(data: Data): Keeper? {
+        val keeper = this.states.remove(data)
+        keeper?.let {
+            onRemoved(keeper)
+            it.clear()
+        }
+        return keeper
+    }
+    fun remove(keeper: Keeper) {
+        remove(keeper.data)
+    }
+
+    protected open fun onAdded(keeper: Keeper) {}
+    protected open fun onRemoved(keeper: Keeper) {}
+
+    protected abstract fun onCreateStateKeeper(data: Data, state: State): Keeper
+
+    protected open fun onDataStateChange(keeper: Keeper, oldState: State, newState: State) {}
+
+    open class Simple<Data, State> : DataStatesCollector<Data, State, StateKeeper.Default<Data, State>>() {
+        override fun onCreateStateKeeper(data: Data, state: State): StateKeeper.Default<Data, State> {
+            return StateKeeper.Default(data, state)
+        }
+    }
+
+}
+
+interface StateKeeper<Data, State> {
+
+    val data: Data
+    var state: State
+    fun observe(observer: (oldState: State, newState: State) -> Unit): Observing
+    fun clear()
+
+    open class Default<Data, State>(data: Data, protected val initState: State) : StateKeeper<Data, State> {
+
+        override val data: Data = data
+        override var state: State = initState
+            set(value) {
+                if (field == value) return
+                val old = field
+                field = value
+                onStateChange(old, field)
+                observers?.let {
+                    it.forEach { observer -> observer.invoke(old, field) }
+                }
+            }
+
+        private var observers: MutableSet<((oldState: State, newState: State) -> Unit)>? = null
+
+        protected open fun onStateChange(oldState: State, newState: State) {}
+
+        override fun observe(observer: (oldState: State, newState: State) -> Unit): Observing {
+            if (observers == null) observers = mutableSetOf()
+            observers!!.add(observer)
+            observer.invoke(state, state)
+            return object : Observing {
+                override fun remove() {
+                    observers?.remove(observer)
+                }
+            }
+        }
+
+        override fun clear() {
+            state = initState
+            observers?.clear()
+        }
+
+    }
+
+}
