6731deb1ed4c63c3d7c1a1b8c2f55bd16a82502c
qeGame <QeGame69@gmail.com>
2021-02-26 23:59:33 +0500

    25.02.2021 ContentUi v6

--- tools/src/main/java/com/kekadoc/tools/content/ui/ContentUI.kt
+++ tools/src/main/java/com/kekadoc/tools/content/ui/ContentUI.kt
@@ -2,6 +2,7 @@ package com.kekadoc.tools.content.ui
 
 import com.kekadoc.tools.fraction.Fraction
 import com.kekadoc.tools.fraction.FractionObserver
+import com.kekadoc.tools.value.ValueUtils
 
 interface ContentUI {
 
@@ -19,6 +20,27 @@ interface ContentUI {
     fun isShown(): Boolean
     fun show()
     fun hide()
+
+    open class SimpleInstance : ContentUI {
+
+        private var shown = false
+
+        protected open fun onShown() {}
+        protected open fun onHide() {}
+
+        override fun isShown() = shown
+        override fun show() {
+            if (shown) return
+            shown = true
+            onShown()
+        }
+        override fun hide() {
+            if (!shown) return
+            shown = false
+            onHide()
+        }
+
+    }
 
     interface Loading : ContentUI, Fraction.Mutable {
 
@@ -53,6 +75,37 @@ interface ContentUI {
         override fun getFraction(): Double
         override fun setFraction(fraction: Double)
 
+        open class SimpleInstance : ContentUI.SimpleInstance(), Loading {
+
+            private var progress = 0.0
+
+            protected open fun onComplete() {}
+            protected open fun onProgress(old: Double, progress: Double) {}
+
+            override fun complete() {
+                setFraction(1.0)
+                onComplete()
+            }
+
+            override fun getFraction(): Double = progress
+
+            override fun setFraction(fraction: Double) {
+                if (progress == fraction) return
+                ValueUtils.setValueInRange(0.0, 1.0, fraction, object : ValueUtils.RangeChangeEvents<Double> {
+                    override fun onChange(oldValue: Double, newValue: Double) {
+                        progress = newValue
+                        onProgress(oldValue, newValue)
+                    }
+                    override fun onMax() {
+                        complete()
+                    }
+                    override fun onMin() {}
+                    override fun onOverflow(overflow: Double) {}
+                })
+            }
+
+        }
+
     }
 
     interface Message : ContentUI {
@@ -62,6 +115,7 @@ interface ContentUI {
         }
 
         object Empty : Message {
+
             override fun isShown(): Boolean = false
             override fun show() {}
             override fun hide() {}
@@ -71,10 +125,20 @@ interface ContentUI {
             override fun setText(text: CharSequence?) {
 
             }
+
         }
 
         fun getText(): CharSequence?
         fun setText(text: CharSequence?)
+
+        open class SimpleInstance(var message: CharSequence? = null) : ContentUI.SimpleInstance(), Message {
+
+            override fun getText(): CharSequence? = message
+            override fun setText(text: CharSequence?) {
+                message = text
+            }
+
+        }
 
     }
 
